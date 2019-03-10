f5d0f2ed87429a42502d148f8bdf3fe346ad1bf8
kerry.wang <wrasdf@gmail.com>
2019-03-10 22:30:27 +1100

    Update cfn runner logic

---
+++ utils/aws.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const AWS = require('aws-sdk'),
+      utils = require('./utility'),
+      cfn = new AWS.CloudFormation({
+        'apiVersion': '2010-05-15',
+        "region": 'ap-southeast-2'
+      })
+
+function createCFNStack(name, cfnFile, callback) {
+  console.log(`Craete CFN $(name)`)
+  return cfn.createStack({
+    'StackName': name,
+    'TemplateBody': utils.readfile(cfnFile),
+    'Capabilities': [ 'CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM' ]
+  }, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    }else {
+      if (callback) {
+        return callback(data)
+      }
+      console.log(data)
+    }
+  })
+}
+
+function deleteCFNStack(name, callback) {
+  console.log(`Delete CFN $(name)`)
+  cfn.deleteStack({
+    'StackName': name
+  }, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    }else {
+      if (callback) {
+        return callback(data)
+      }
+      console.log(data)
+    }
+  })
+}
+
+function updateCFNStack(name, cfnFile, callback) {
+  console.log(`Update CFN $(name)`)
+  cfn.updateStack({
+    'StackName': name,
+    'TemplateBody': utils.readfile(cfnFile),
+    'Capabilities': [ 'CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM' ]
+  }, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    }else {
+      if (callback) {
+        return callback(data)
+      }
+      console.log(data)
+    }
+  })
+}
+
+function getCFNStack(name, callback) {
+  cfn.listStacks({}, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    }else {
+      callback(data.StackSummaries.filter(stack => {
+        return stack.StackName === name && stack.StackStatus !== 'DELETE_COMPLETE'
+      }))
+    }
+  })
+}
+
+function waitCFNStack(name, callback) {
+
+  console.log(`Wait For Deleting CFN ${name} ...`)
+  const waitInterval = setInterval(() => {
+    getCFNStack(name, stacks => {
+      if (stacks.length === 0 || /^.*COMPLETE.*$/g.test(stacks[0].StackStatus)) {
+        clearInterval(waitInterval)
+        return callback()
+      }
+    })
+  }, 500)
+
+}
+
+function isStackInProcess(name, callback) {
+
+  getCFNStack(name, stacks => {
+    if (/^.*IN_PROGRESS.*$/g.test(stacks[0].StackStatus)) {
+      return callback(true, stacks[0])
+    }
+    return callback(false, stacks[0] ? stacks[0]: {})
+  })
+
+}
+
+function deployCFNStack(name, cfnFile) {
+
+  const deadStatus = ['CREATE_FAILED', 'ROLLBACK_COMPLETE']
+  getCFNStack(name, stacks => {
+
+    if (stacks.length === 0) {
+      return createCFNStack(name, cfnFile)
+    }
+
+    const currentStack = stacks[0]
+    if (deadStatus.indexOf(currentStack.StackStatus) !== -1) {
+      console.log(`Delete CFN ${name}`)
+      return deleteCFNStack(name, () => {
+        waitCFNStack(name, () => createCFNStack(name, cfnFile))
+      })
+    }
+
+    isStackInProcess(name, (inProcess, stack) => {
+      if (inProcess) {
+        return console.log(`CFN ${stack.StackName} still ${stack.StackStatus}`)
+      }
+      updateCFNStack(name, cfnFile)
+    })
+
+  })
+
+}
+
+module.exports = {
+  deployCFNStack
+}
--- utils/core.js
+++ utils/core.js
@@ -3,6 +3,7 @@
 const parser = require('./parser'),
       utils = require('./utility'),
       Mustache = require('mustache'),
+      aws = require('./aws'),
       buildPath = '_cfns_build'
 
 function cfnCompile(cliObj) {
@@ -27,33 +28,15 @@ function cfnValidate(cliObj) {
     const file = `${process.cwd()}/${buildPath}/${component}.yaml`
     utils.exec(`aws cloudformation validate-template --template-body file://${file}`)
   })
-
 }
+
 
 function cfnApply(cliObj) {
   cfnCompile(cliObj)
   cliObj.components.map(component => {
     const file = `${process.cwd()}/${buildPath}/${component}.yaml`
     const name = (typeof cliObj.name === "function") ? `${component}-stack` : `${cliObj.name}-${component}-stack`
-    const deadStatus = ['CREATE_FAILED', 'ROLLBACK_COMPLETE']
-
-    // TODO Need to be refactored
-
-    const status = utils.exec(`aws cloudformation describe-stacks --stack-name ${name}`, {silent: true})
-    if (status != "") {
-      JSON.parse(status.stdout).Stacks
-      .filter(stack => stack.StackName === name )
-      .map(stack => {
-        if (deadStatus.indexOf(stack.StackStatus) !== -1){
-          utils.exec(`aws cloudformation delete-stack --stack-name ${name}`)
-        }
-      })
-    }
-// kube-vpc-stack/933f98a0-427b-11e9-9a23-0af0093ead5a is in DELETE_IN_PROGRESS state and can not be updated.
-// An error occurred (ValidationError) when calling the DescribeStacks operation: Stack with id kube-vpc-stack does not exist
-    // TODO wait for
-
-    utils.exec(`aws cloudformation deploy --stack-name ${name} --template-file ${file} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM`)
+    aws.deployCFNStack(name, file)
   })
 }
 
